97d3f8a0d674ffff53267dacc79e53513303a7bb
vicenzoanza <viceanza2010@hotmail.com>
2023-08-24 00:26:33 -0300

    tasks

--- API/0-gather_data_from_an_API.py
+++ API/0-gather_data_from_an_API.py
@@ -5,27 +5,27 @@ import sys
 
 if __name__ == "__main__":
 
-if len(argv) == 1:
+    if len(argv) == 1:
     
-    employee_id = argv[1]
-    
-    request_user = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    )
-    
-    request_user_json = json.loads(request_user.text)
-    
-    total_user_request = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    )
-    total_user_request_json = json.loads(total_user_request.text)
-    finished = 0
-    total = 0
-    
-    for tasks in total_user_request_json:
-        if task["completed"]:
-            ready += 1
-        else:
-            not_ready += 1
-    
-    print f"Employee {'name'} is done with tasks({ready}/{not_ready + ready})"
+        employee_id = argv[1]
+        
+        request_user = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+        )
+        
+        request_user_json = json.loads(request_user.text)
+        
+        total_user_request = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+        )
+        total_user_request_json = json.loads(total_user_request.text)
+        finished = 0
+        total = 0
+        
+        for tasks in total_user_request_json:
+            if task["completed"]:
+                ready += 1
+            else:
+                not_ready += 1
+        
+        print f"Employee {'name'} is done with tasks({ready}/{not_ready + ready})"
