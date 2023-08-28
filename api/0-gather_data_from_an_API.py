820d57ce09e9b75dd6adc8758b17115009dab24e
vicenzoanza <viceanza2010@hotmail.com>
2023-08-27 23:32:24 -0300

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
+""""""
 import requests
 import json
 from sys import argv
 if __name__ == "__main__":
 
-    if len(argv) == 1:
+    if len(argv) != 2:
+        exit()
+
     
-        employee_id = argv[1]
-        
-        request_user = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        )
-        
-        request_user_json = json.loads(request_user.text)
-        
-        total_user_request = requests.get(
-            f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        )
-        total_user_request_json = json.loads(total_user_request.text)
-        finished = 0
-        total = 0
-        
-        for tasks in total_user_request_json:
-            if task["completed"]:
-                ready += 1
-            else:
-                not_ready += 1
-                
-        print (f"Employee {'name'} is done with tasks({ready}/{not_ready + ready})")
-        for task in total_user_request_json:
-            if task["completed"]:
-                print(f'\t {task["title"]}')
+    employee_id = argv[1]
+
+    request_user = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    request_user_json = json.loads(request_user.text)
+
+    total_user_request = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    total_user_request_json = json.loads(total_user_request.text)
+
+    completed_tasks = []
+    total_tasks = len(total_user_request_json)
+
+    for task in total_user_request_json:
+        if task["completed"]:
+            completed_tasks.append(task["title"])
+
+    num_completed_tasks = len(completed_tasks)
+
+    print(f"Employee {request_user_json['name']} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    for task_title in completed_tasks:
+        print(f"\t {task_title}")
